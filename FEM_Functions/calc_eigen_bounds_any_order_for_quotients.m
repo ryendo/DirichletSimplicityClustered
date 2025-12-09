15681b306732dc62ca0565edaf034e1df289191b
Ryoki Endo <119128670+ryendo@users.noreply.github.com>
2025-12-10 08:58:11 +0900

    Update calc_eigen_bounds_any_order_for_quotients.m

--- FEM_Functions/calc_eigen_bounds_any_order_for_quotients.m
+++ FEM_Functions/calc_eigen_bounds_any_order_for_quotients.m
@@ -1,11 +1,12 @@
-function [LA_eig, LA_eigf, LA_A, LA_M, A_inner_xx, A_inner_xy, A_inner_yy, bd_dof_idx] = calc_eigen_bounds_any_order_for_quotients(tri_intval,N,ord)
-    
+function [LA_eig, LA_eigf, LA_A, LA_M, A_inner_xx, A_inner_xy, A_inner_yy, bd_dof_idx] = calc_eigen_bounds_any_order_for_quotients(tri_intval,ord)
+
     neig = 3;
-    mesh=get_mesh_for_cg(tri_intval,N);
-    vert = mesh.nodes;
-    edge = mesh.edges;
-    tri  = mesh.elements;
-    bd   = mesh.edges(is_edge_on_bdry(mesh,N),:);
+    mesh_size = 0.03125;
+    mesh = make_mesh_by_gmsh(a1, b1, mesh_size);        
+    vert = I_intval(mesh_rho.nodes);
+    edge = mesh_rho.edges;
+    tri  = mesh_rho.elements;
+    bd   = mesh_rho.boundary_edges;
     
     Lagrange_order = ord;
     [LA_eig, LA_eigf, ~, LA_A, LA_M, A_inner_xx, A_inner_xy, A_inner_yy, bd_dof_idx] = laplace_eig_lagrange_detailed(Lagrange_order, vert, edge, tri, bd, neig);
